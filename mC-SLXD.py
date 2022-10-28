5b4fbb9a8b2234264c48c2886204869235249294
Noah-Sokol <94477428+Noah-Sokol@users.noreply.github.com>
2022-10-28 08:48:55 -0400

    Update mC-SLXD.py

--- mC-SLXD.py
+++ mC-SLXD.py
@@ -1,33 +1,39 @@
 '''
 Created on Oct 24, 2022
+Last edited 10/28/22
 
-@author: NSokol25
+@author: Noah Sokol
 '''
+'''
+    Creates a substring then moves certain letters inside that substring
 
+#Arg:
+ 
+    word - the word that is being changed
+    s - starting position of substring
+    l - length of substring
+    x - how many characters are being moved from D
+    d - which direction the characters come from
+    
+#returns:
+
+    output - the word with the parameters acted on
+
+'''
 def mC(word, s,l,x,d):
     s = int(s)
     l = int(l)
     x = int(x)
-    substring = word[s-1:s+l-1]
+    substring = word[s-1:s+l-1]                             #Creates the substring
     if d == 'L' or d == 'l':
-        moving = substring[:x]
+        moving = substring[:x]                              #which characters are moving
         notmoving = substring[x:]
         newsubstring = notmoving + moving
-        output = word.replace(substring,newsubstring)
+        output = word.replace(substring,newsubstring)       #gets the output
         return output
     if d == 'R' or d == 'r':
-        moving = substring[l- x:]
-        notmoving = substring[:l-x:]
+        moving = substring[l- x:]                           #moving is gotten by substring how many characters 
+        notmoving = substring[:l-x:]                        # are being moved starting from the length of the substring
         newsubstring = moving + notmoving
         output = word.replace(substring,newsubstring)
         return output
-    
-def main():
-    word = input('what word would would you like to use: ')
-    s = int(input('where would you like to start your substring: '))
-    l = int(input('how long would you like your substring to be: '))
-    x = int(input('how many characters would you like to move: '))
-    d = input('which direction would you like to move from (R) or (L)')
-    output = mC(word, s, l,x,d)
-    print(output)
-main()
